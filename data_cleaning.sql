03e92f8cefcae6e1b852fde9cad1e61d6f85a8c9
Li <luo.li086@gmail.com>
2025-06-22 13:50:43 +0200

    Convert column tags to type text array

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,18 +1,27 @@
 -- Data Cleaning
 
 -- 1. Copy original data for data cleaning
-CREATE TABLE VIDEOS_CLEANED AS
-SELECT * FROM RAW_VIDEOS;
+CREATE TABLE videos_cleaned AS
+SELECT * FROM raw_videos;
 
 
 -- 2. Check data structure
-SELECT * FROM VIDEOS_CLEANED LIMIT 10;
+SELECT * FROM videos_cleaned LIMIT 10;
 
 
--- 3. Check the duplicate in table videos_cleaned
+-- 3. check data type of columns
+SELECT
+	column_name, data_type
+FROM
+	information_schema.columns
+WHERE
+	table_name = 'videos_cleaned';
+
+
+-- 4. Check the duplicate in table videos_cleaned
 SELECT a.*
-FROM VIDEOS_CLEANED a
-JOIN VIDEOS_CLEANED b
+FROM videos_cleaned a
+JOIN videos_cleaned b
 ON 
 	a.video_id = b.video_id
 WHERE
@@ -20,26 +29,30 @@ WHERE
 -- Result: there is no duplicate
 
 
--- 4. Trim whitespace in text columns
-UPDATE VIDEOS_CLEANED
+-- 5. Trim whitespace in text columns
+UPDATE videos_cleaned
 SET 
 	video_id = TRIM(video_id),
 	title = TRIM(title),
     tags = TRIM(tags);
 	
 
--- 5. Handle NULLs in numerical columns
-UPDATE VIDEOS_CLEANED
+-- 6. Handle NULLs in numerical columns
+UPDATE videos_cleaned
 SET 
 	views = COALESCE(views,0),
 	likes = COALESCE(likes,0),
 	comments = COALESCE(comments,0);
 
 
--- 6. check data type of columns
-SELECT
-	column_name, data_type
-FROM
-	information_schema.columns
-WHERE
-	table_name = 'videos_cleaned';
+
+
+
+-- 7. Convert tags to text array
+ALTER TABLE videos_cleaned
+ADD COLUMN tags_array text[];
+
+UPDATE videos_cleaned
+SET
+	tags_array = string_to_array(REPLACE(REPLACE(REPLACE(tags, '[',''),']',''),'''',''),', '); 
+
